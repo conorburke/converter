7d8e6d7b0cb92535338eab8b59ff10c30400f83e
conorburke <cjburke89@gmail.com>
2019-04-04 19:09:20 -0700

    moved to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,42 @@
-pub mod convert {
-    pub fn printconv() -> i32 {
-        println!("in convert!");
-        2
+pub mod derives {
+    use serde::Serialize;
+    #[derive(Debug, Serialize)]
+    pub struct Record {
+        pub name: String,
+        pub universe: String
     }
 }
 
 pub mod gets {
+    use calamine::{open_workbook, Reader, Xlsx};
+    use std::error::Error;
+    use std::fs::File;
+    pub fn converter(args: Vec<String>) -> Result<(), Box<Error>>{
+    let input = Input::new(&args);
+
+        let buffer = File::create(input.outputname)?;
+        let mut wtr = csv::Writer::from_writer(buffer); 
+
+        let mut excel: Xlsx<_> = open_workbook(input.filename).unwrap();
+        if let Some(Ok(r)) = excel.worksheet_range("Sheet1") {
+            for row in r.rows() {
+                wtr.serialize(super::derives::Record {
+                name: row[0].to_string(),
+                universe: row[1].to_string(),
+                })?;
+            }
+        }
+        wtr.flush()?;
+        Ok(())
+    }
+
+
+
     #[derive(Debug)]
     pub struct Input {
         pub filename: String,
         pub outputname: String
     }
-
-    // fn read_inputs(args: &[String]) -> Input {
-    //     let filename = args[1].clone();
-
-    //     Input { ilename }
-    // }
 
     impl Input {
         pub fn new(args: &[String]) -> Input {
@@ -33,10 +53,5 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn it_prints() {
-        assert_eq!(super::convert::printconv(), 2);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,11 @@
-use calamine::{open_workbook, Reader, Xlsx};
 use std::env;
-use std::error::Error;
-use std::fs::File;
 
 mod lib;
 
-fn main() -> Result<(), Box<Error>>{
-    lib::convert::printconv();
+fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     if args.len() != 3 {
         panic!("I need a source file (xlsx) and a destination file (.csv)");
     }
-    let input = lib::gets::Input::new(&args);
-    println!("{:?}", input);
-
-    let buffer = File::create(input.outputname)?;
-    let mut wtr = csv::Writer::from_writer(buffer); 
-
-    let mut excel: Xlsx<_> = open_workbook(input.filename).unwrap();
-    if let Some(Ok(r)) = excel.worksheet_range("Sheet1") {
-        for row in r.rows() {
-            println!("{:?}", row);
-            let mut vec: Vec<String> = Vec::new();
-            for x in row {
-              vec.push(x.to_string());
-              println!("{}", x);
-            }
-            wtr.write_record(vec)?;
-        }
-    }
-    wtr.flush()?;
-    Ok(())
+    lib::gets::converter(args).expect("shit broke");
 }
