1fe3bdfb1e5e5a29c98f20ceae37e51982e442e3
conorburke <cjburke89@gmail.com>
2019-04-04 19:43:26 -0700

    support multiple sheets if you know them ahead of time

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,63 @@
-pub mod derives {
-    use serde::Serialize;
-    #[derive(Debug, Serialize)]
-    pub struct Record {
-        pub name: String,
-        pub universe: String
-    }
-}
+// a struct for automatic titles if I want that in future
+
+// pub mod derives {
+//     use serde::Serialize;
+//     #[derive(Debug, Serialize)]
+//     pub struct Record {
+//         pub name: String,
+//         pub universe: String,
+//     }
+// }
 
 pub mod gets {
     use calamine::{open_workbook, Reader, Xlsx};
     use std::error::Error;
     use std::fs::File;
-    pub fn converter(args: Vec<String>) -> Result<(), Box<Error>>{
-    let input = Input::new(&args);
+    pub fn converter(args: Vec<String>) -> Result<(), Box<Error>> {
+        let input = Input::new(&args);
 
         let buffer = File::create(input.outputname)?;
-        let mut wtr = csv::Writer::from_writer(buffer); 
+        let mut wtr = csv::Writer::from_writer(buffer);
+
+        let buffer2 = File::create("output2.csv")?;
+        let mut wtr2 = csv::Writer::from_writer(buffer2);
 
         let mut excel: Xlsx<_> = open_workbook(input.filename).unwrap();
         if let Some(Ok(r)) = excel.worksheet_range("Sheet1") {
             for row in r.rows() {
-                wtr.serialize(super::derives::Record {
-                name: row[0].to_string(),
-                universe: row[1].to_string(),
-                })?;
+                let mut vec: Vec<String> = Vec::new();
+                for x in row {
+                    vec.push(x.to_string());
+                }
+                wtr.write_record(vec)?;
             }
         }
+        // if let Some(Ok(r)) = excel.worksheet_range("Sheet2") {
+        //     for row in r.rows() {
+        //         wtr.serialize(super::derives::Record {
+        //             name: row[0].to_string(),
+        //             universe: row[1].to_string(),
+        //         })?;
+        //     }
+        // }
+        if let Some(Ok(r)) = excel.worksheet_range("Sheet2") {
+            for row in r.rows() {
+                let mut vec: Vec<String> = Vec::new();
+                for x in row {
+                    vec.push(x.to_string());
+                }
+                wtr2.write_record(vec)?;
+            }
+        }
+
         wtr.flush()?;
         Ok(())
     }
 
-
-
     #[derive(Debug)]
     pub struct Input {
         pub filename: String,
-        pub outputname: String
+        pub outputname: String,
     }
 
     impl Input {
@@ -43,7 +65,10 @@ pub mod gets {
             let filename = args[1].clone();
             let outputname = args[2].clone();
 
-            Input { filename, outputname }
+            Input {
+                filename,
+                outputname,
+            }
         }
     }
 }
